07a9ed1b622aae00d46f145b0d839959d2be4fd2
mniang04 <mohametniang4@gmail.com>
2021-11-24 15:29:33 +0000

    new config

--- lib/screens/iotscreen.dart
+++ lib/screens/iotscreen.dart
@@ -15,60 +15,141 @@ class IotScreen extends StatefulWidget {
 
 class _IotScreenState extends State<IotScreen> {
   @override
+  // ignore: override_on_non_overriding_member
   bool value = false;
-  final dbRef=FirebaseDatabase.instance.reference();
+  final dbRef = FirebaseDatabase.instance.reference();
 
   onUpDate() {
     setState(() {
-      value =! value;
+      value = !value;
     });
   }
 
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown
-    ]);
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return Scaffold(
       body: Column(
         children: [
           Padding(
-            padding: EdgeInsets.all(18),
+            padding: EdgeInsets.all(38),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.center,
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Icon(
-                  Icons.clear_all,
-                  color: Colors.white,
-                ),
+                //Icon(
+                 // Icons.clear_all,
+                 // color: Colors.white,
+               // ),
                 Text(
-                  "MY FIELD",
+                  "Smart_Farming",
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-                Icon(Icons.settings)
+                //Icon(Icons.settings)
               ],
             ),
           ),
 
-          //data temperature
-          SizedBox(height: 20),
+          SizedBox(height: 0),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Column(
                 children: [
-                  Padding(padding: const EdgeInsets.all(8.0),
-                  child: Text("Temperature",  //type of data
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      "météorologique ambiante", //type of data
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                     ),
                   ),
-                  Padding(padding: const EdgeInsets.all(8.0),
-                  child: Text("20", //data
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
+                ],
+              ),
+            ],
+          ),
+
+          //data temperature et humidite ambiante
+          SizedBox(height: 0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Temperature", //type of data
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "20 °C", //data
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Humidité", //type of data
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "80%", //data
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "luminosité", //type of data
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "80%", //data
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Vents", //type of data
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "10m/s", //data
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
                     ),
                   ),
                 ],
@@ -76,45 +157,75 @@ class _IotScreenState extends State<IotScreen> {
             ],
           ),
 
-          //data humidity
-          SizedBox(height: 20),
-          Column(
+          SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
                 children: [
-                  Padding(padding: const EdgeInsets.all(8.0),
-                  child: Text("Hymidité", //type of data
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Padding(padding: const EdgeInsets.all(8.0),
-                  child: Text("80%", //data
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      "Données sol", //type of data
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 80,),
-              FloatingActionButton.extended(
-                onPressed: () {
-                  onUpDate();
+            ],
+          ),
+          //data sol
+          SizedBox(height: 0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Humidité sol", //type of data
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "80%", //data
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
 
-                  writeData();
-                }, 
-                label:value? Text("OPEN") : Text("CLOSE"),
-                elevation: 20,
-                backgroundColor:value? Colors.yellow:Colors.blue,
-                icon: value ? Icon(Icons.closed_caption) : Icon(Icons.closed_caption_off),
-                )
+          SizedBox(
+            height: 80,
+          ),
+          FloatingActionButton.extended(
+            onPressed: () {
+              onUpDate();
+
+              writeData();
+            },
+            label: value ? Text("OPEN") : Text("CLOSE"),
+            elevation: 20,
+            backgroundColor: value ? Colors.yellow : Colors.blue,
+            icon: value
+                ? Icon(Icons.closed_caption)
+                : Icon(Icons.closed_caption_off),
+          )
         ],
       ),
     );
   }
 
-   Future <void> writeData() async {
+  Future<void> writeData() async {
     dbRef.child("Data").set({"aHymidity:": 0, "aTemperature:": 0});
     dbRef.child("ValveState").set({"switch": !value});
-  } 
+  }
 }
